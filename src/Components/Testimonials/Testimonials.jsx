ef8ea2dbdd9833e740f31f9c5ca53a09d6ef765f
MADUFOROVIVIAN <vivianmaduforo85@gmail.com>
2024-05-24 14:36:07 +0100

    add

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import style from "/.Testimonials.module.css"
-
+import style from "./Testimonials.module.css"
+import client from  "../../Assests/client.png"
 const Testimonials = () => {
   return (
     <div className={style.container}>
